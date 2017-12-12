76d3798cbf69c04d1d98c00953691c289d0f7593
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2017-12-12 19:42:10 +0100

    added simple allocator integration test

---
+++ include/gbBase/Allocator.hpp
@@ -0,0 +1,17 @@
+#ifndef GHULBUS_LIBRARY_INCLUDE_GUARD_BASE_ALLOCATOR_HPP
+#define GHULBUS_LIBRARY_INCLUDE_GUARD_BASE_ALLOCATOR_HPP
+
+/** @file
+*
+* @brief Custom allocators.
+* @author Andreas Weis (der_ghulbus@ghulbus-inc.de)
+*/
+#include <gbBase/config.hpp>
+
+#include <gbBase/Allocator/LinearMemoryResource.hpp>
+#include <gbBase/Allocator/StatefulAllocator.hpp>
+
+namespace GHULBUS_BASE_NAMESPACE
+{
+}
+#endif
---
+++ test/TestAllocator.cpp
@@ -0,0 +1,22 @@
+#include <gbBase/Allocator.hpp>
+
+#include <catch.hpp>
+
+#include <algorithm>
+#include <vector>
+
+
+TEST_CASE("Allocator integration test")
+{
+    using namespace GHULBUS_BASE_NAMESPACE;
+
+    Allocator::Storage::Dynamic storage(1024);
+    Allocator::LinearMemoryResource<Allocator::Storage::Dynamic> resource(storage);
+    Allocator::StatefulAllocator<double, decltype(resource)> allocator(resource);
+
+    std::vector<double, decltype(allocator)> my_vector(allocator);
+    my_vector.resize(100, 42.5);
+    CHECK(resource.getFreeMemory() <= 1024 - (100 * sizeof(double)));
+    CHECK(reinterpret_cast<std::byte*>(my_vector.data()) - storage.get() < resource.getFreeMemory());
+    CHECK(std::all_of(begin(my_vector), end(my_vector), [](double d) { return d == 42.5; }));
+}
