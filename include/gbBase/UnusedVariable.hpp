1aa8ae2ee6f1174cb4d6f3ddac6a865b0858323e
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2017-12-13 18:26:57 +0100

    added documentation

--- include/gbBase/UnusedVariable.hpp
+++ include/gbBase/UnusedVariable.hpp
@@ -1,6 +1,15 @@
 #ifndef GHULBUS_LIBRARY_INCLUDE_GUARD_BASE_UNUSED_VARIABLE_HPP
 #define GHULBUS_LIBRARY_INCLUDE_GUARD_BASE_UNUSED_VARIABLE_HPP
 
+/** @file
+*
+* @brief Macro for marking unused variables.
+* @author Andreas Weis (der_ghulbus@ghulbus-inc.de)
+*/
+
+/** Use this to mark unused variables in code.
+ * This should suppress any unused variable warnings from the compiler.
+ */
 #define GHULBUS_UNUSED_VARIABLE(x) (void)x
 
 #endif
