ed4d8fb9cb8f24a55882a705db851a774f1d9d79
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2017-12-16 15:00:14 +0100

    first shot at ring allocation strategy

---
+++ include/gbBase/Allocator/AllocationStrategyRing.hpp
@@ -0,0 +1,201 @@
+#ifndef GHULBUS_LIBRARY_INCLUDE_GUARD_BASE_ALLOCATOR_ALLOCATION_STRATEGY_RING_HPP
+#define GHULBUS_LIBRARY_INCLUDE_GUARD_BASE_ALLOCATOR_ALLOCATION_STRATEGY_RING_HPP
+
+/** @file
+*
+* @brief Ring allocation strategy.
+* @author Andreas Weis (der_ghulbus@ghulbus-inc.de)
+*/
+#include <gbBase/config.hpp>
+
+#include <gbBase/Allocator/DebugPolicy.hpp>
+#include <gbBase/Allocator/Storage.hpp>
+
+#include <gbBase/Assert.hpp>
+
+#include <cstddef>
+#include <limits>
+#include <memory>
+#include <new>
+
+namespace GHULBUS_BASE_NAMESPACE
+{
+namespace Allocator
+{
+namespace AllocationStrategy
+{
+
+/** The ring allocation strategy.
+ */
+template<typename Storage_T, typename Debug_T = Allocator::DebugPolicy::AllocateDeallocateCounter>
+class Ring : private Debug_T {
+public:
+    /** Header used for internal bookkeeping of allocations.
+     * Each block of memory returned by allocate() is preceded by a header.
+     */
+    class Header {
+    private:
+        Header* m_nextHeader;           ///< Pointer to the next header in the list.
+        Header* m_previousHeader;       ///< Pointer to the previous header in the list.
+        bool m_wasFreed;
+
+    public:
+
+        Header(Header* previous_header)
+            :m_nextHeader(nullptr), m_previousHeader(previous_header), m_wasFreed(false)
+        {}
+
+        void setNextHeader(Header* header)
+        {
+            GHULBUS_PRECONDITION_DBG(header && !m_nextHeader);
+            m_nextHeader = header;
+        }
+
+        void clearPreviousHeader()
+        {
+            GHULBUS_PRECONDITION_DBG(m_previousHeader);
+            m_previousHeader = nullptr;
+        }
+
+        void clearNextHeader()
+        {
+            GHULBUS_PRECONDITION_DBG(m_nextHeader);
+            m_nextHeader = nullptr;
+        }
+
+        Header* nextHeader() const
+        {
+            return m_nextHeader;
+        }
+
+        Header* previousHeader() const
+        {
+            return m_previousHeader;
+        }
+
+        void markFree()
+        {
+            m_wasFreed = true;
+        }
+
+        bool wasFreed() const
+        {
+            return m_wasFreed;
+        }
+    };
+private:
+    Storage_T* m_storage;
+    Header* m_topHeader;
+    Header* m_bottomHeader;
+    std::size_t m_freeMemoryOffset;
+
+public:
+    Ring(Storage_T& storage) noexcept
+        :m_storage(&storage), m_topHeader(nullptr), m_bottomHeader(nullptr), m_freeMemoryOffset(0)
+    {}
+
+    std::byte* allocate(std::size_t n, std::size_t alignment)
+    {
+        // we have to leave room for the header before the pointer that we return
+        std::size_t free_space = getFreeMemoryContiguous(m_freeMemoryOffset);
+        bool const out_of_memory = (free_space < sizeof(Header));
+        free_space -= (out_of_memory) ? 0 : sizeof(Header);
+        void* ptr = reinterpret_cast<void*>(m_storage->get() + getFreeMemoryOffset() + sizeof(Header));
+        // the alignment has to be at least alignof(Header) to guarantee that the header is
+        // stored at its natural alignment.
+        // As usual, this assumes that all alignments are powers of two.
+        if(out_of_memory || (!std::align(std::max(alignment, alignof(Header)), n, ptr, free_space))) {
+            // we are out of memory, so try wrap around the ring
+            if(isWrappedAround() || ((free_space = getFreeMemoryContiguous(0)) < sizeof(Header))) {
+                // already wrapped, or not enough space even after wrapping
+                throw std::bad_alloc();
+            }
+            free_space -= sizeof(Header);
+            ptr = reinterpret_cast<void*>(m_storage->get() + sizeof(Header));
+            if(!std::align(std::max(alignment, alignof(Header)), n, ptr, free_space)) {
+                // not enough free space in the beginning either
+                throw std::bad_alloc();
+            }
+        }
+        std::byte* ret = reinterpret_cast<std::byte*>(ptr);
+
+        // setup a header in the memory region immediately preceding ret
+        Header* new_header = new (ret - sizeof(Header)) Header(m_topHeader);
+        if(m_topHeader == nullptr) {
+            m_bottomHeader = new_header;
+        } else {
+            m_topHeader->setNextHeader(new_header);
+        }
+        m_topHeader = new_header;
+        GHULBUS_ASSERT_DBG(m_bottomHeader);
+        m_freeMemoryOffset = (ret - m_storage->get()) + n;
+
+        this->onAllocate(n, alignment, ret);
+        return ret;
+    }
+
+    void deallocate(std::byte* p, std::size_t n)
+    {
+        this->onDeallocate(p, n);
+
+        // mark the deallocated block as freed in the header
+        Header* header_start = reinterpret_cast<Header*>(p - sizeof(Header));
+        header_start->markFree();
+
+        // advance the top header to the left until it no longer points to a freed header
+        while(m_topHeader && (m_topHeader->wasFreed())) {
+            header_start = m_topHeader;
+            m_topHeader = header_start->previousHeader();
+            if(m_topHeader) {
+                m_topHeader->clearNextHeader();
+                m_freeMemoryOffset = (reinterpret_cast<std::byte*>(header_start) - m_storage->get());
+            } else {
+                GHULBUS_ASSERT_DBG(m_bottomHeader == header_start);
+                m_bottomHeader = nullptr;
+                m_freeMemoryOffset = 0;
+            }
+            header_start->~Header();
+        }
+
+        // advance the bottom header to the right until it no longer points to a freed header
+        while(m_bottomHeader && (m_bottomHeader->wasFreed())) {
+            header_start = m_bottomHeader;
+            m_bottomHeader = header_start->nextHeader();
+            if(m_bottomHeader) { m_bottomHeader->clearPreviousHeader(); }
+            header_start->~Header();
+        }
+    }
+
+    /** Returns the offset in bytes from the start of the storage to the start of the free memory region.
+     */
+    std::size_t getFreeMemoryOffset() const noexcept
+    {
+        return m_freeMemoryOffset;
+    }
+
+    bool isWrappedAround() const
+    {
+        // we are wrapped iff the current offset is left of the bottom header
+        return (m_storage->get() + getFreeMemoryOffset()) <= reinterpret_cast<std::byte*>(m_bottomHeader);
+    }
+
+    /** Get the biggest contiguous block starting from offs.
+     * @pre offs must not point into allocated memory.
+     */
+    std::size_t getFreeMemoryContiguous(std::size_t offs) const noexcept
+    {
+        std::byte* offs_ptr = (m_storage->get() + offs);
+        std::byte* bottom_ptr = reinterpret_cast<std::byte*>(m_bottomHeader);
+        if(bottom_ptr < offs_ptr) {
+            // linear case: free space from offset to end of storage
+            return m_storage->size() - offs;
+        } else {
+            // wrap-around case: free space from offset to bottom header
+            return bottom_ptr - offs_ptr;
+        }
+    }
+};
+}
+}
+}
+#endif
---
+++ test/TestAllocatorAllocationStrategyRing.cpp
@@ -0,0 +1,122 @@
+#include <gbBase/Allocator/AllocationStrategyRing.hpp>
+
+#include <gbBase/Allocator/Storage.hpp>
+
+#include <MockDebugPolicy.hpp>
+#include <MockStorage.hpp>
+
+#include <catch.hpp>
+
+#include <type_traits>
+
+TEST_CASE("Ring Allocation Strategy")
+{
+    using namespace GHULBUS_BASE_NAMESPACE;
+    using testing::MockDebugPolicy;
+    using testing::MockStorage;
+
+    MockStorage storage;
+
+    Allocator::AllocationStrategy::Ring<MockStorage, MockDebugPolicy> ring(storage);
+    using Header = decltype(ring)::Header;
+    MockDebugPolicy::number_on_allocate_calls = 0;
+    MockDebugPolicy::number_on_deallocate_calls = 0;
+    MockDebugPolicy::number_on_reset_calls = 0;
+
+    std::size_t const free_marker = std::numeric_limits<std::size_t>::max();
+
+    SECTION("Allocator Header")
+    {
+        Header h1(nullptr);
+        CHECK(h1.previousHeader() == nullptr);
+        CHECK(h1.nextHeader() == nullptr);
+        CHECK(!h1.wasFreed());
+
+        Header h2(&h1);
+        CHECK(h2.previousHeader() == &h1);
+        CHECK(h2.nextHeader() == nullptr);
+        CHECK(!h2.wasFreed());
+
+        Header h3(nullptr);
+        h2.setNextHeader(&h3);
+        CHECK(h2.nextHeader() == &h3);
+        h2.clearNextHeader();
+        CHECK(h2.nextHeader() == nullptr);
+
+        CHECK(h2.previousHeader() == &h1);
+        h2.clearPreviousHeader();
+        CHECK(h2.previousHeader() == nullptr);
+
+        CHECK(!h2.wasFreed());
+        h2.markFree();
+        CHECK(h2.wasFreed());
+    }
+
+
+    SECTION("Size and offset")
+    {
+        std::byte x;
+        storage.memory_ptr = &x;
+        storage.memory_size = 42;
+        CHECK(ring.getFreeMemoryOffset() == 0);
+        CHECK(ring.getFreeMemoryContiguous(0) == 42);
+    }
+
+    SECTION("Allocate")
+    {
+        std::aligned_storage_t<128, alignof(Header)> as;
+        auto ptr = reinterpret_cast<std::byte*>(&as);
+        storage.memory_ptr = ptr;
+        storage.memory_size = 128;
+
+        CHECK(ring.getFreeMemoryOffset() == 0);
+        auto* const p1 = ring.allocate(16, 1);
+        CHECK(p1 == ptr + sizeof(Header));
+        auto const* p2 = ring.allocate(16, 1);
+        CHECK(p2 == ptr + 2*sizeof(Header) + 16);
+    }
+
+    SECTION("Allocate wrap-around")
+    {
+        std::aligned_storage_t<128, alignof(Header)> as;
+        auto ptr = reinterpret_cast<std::byte*>(&as);
+        storage.memory_ptr = ptr;
+        storage.memory_size = 128;
+
+        CHECK(ring.getFreeMemoryOffset() == 0);
+        auto* const p1 = ring.allocate(64, 1);
+        CHECK(p1 == ptr + sizeof(Header));              // p1 [0 .. 88]
+        auto* const p2 = ring.allocate(16, 1);
+        CHECK(p2 == ptr + 2*sizeof(Header) + 64);       // p2 [88 .. 128]
+
+        CHECK_THROWS_AS(ring.allocate(16, 1), std::bad_alloc);
+        ring.deallocate(p1, 64);
+        auto* const p3 = ring.allocate(16, 1);
+        CHECK(p3 == ptr + sizeof(Header));              // p3 [0 .. 40]
+
+        auto* const p4 = ring.allocate(16, 1);
+        CHECK(p4 == ptr + 2*sizeof(Header) + 16);       // p4 [40 .. 80]
+
+        CHECK_THROWS_AS(ring.allocate(1, 1), std::bad_alloc);
+
+        ring.deallocate(p3, 16);
+        CHECK_THROWS_AS(ring.allocate(1, 1), std::bad_alloc);
+
+        ring.deallocate(p2, 16);
+        CHECK_THROWS_AS(ring.allocate(25, 1), std::bad_alloc);
+        auto* const p5 = ring.allocate(24, 1);
+        CHECK(p5 == ptr + 3*sizeof(Header) + 32);       // p5 [80 .. 128]
+
+        CHECK_THROWS_AS(ring.allocate(17, 1), std::bad_alloc);
+        auto* const p6 = ring.allocate(16, 1);
+        CHECK(p6 == ptr + sizeof(Header));              // p6 [0 .. 40]
+
+        CHECK_THROWS_AS(ring.allocate(0, 1), std::bad_alloc);
+
+        ring.deallocate(p5, 24);
+        ring.deallocate(p6, 16);
+        ring.deallocate(p4, 16);
+        CHECK(ring.getFreeMemoryOffset() == 0);
+        CHECK(ring.getFreeMemoryContiguous(0) == 128);
+    }
+}
