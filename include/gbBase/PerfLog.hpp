e971449c44ce4bca9d216801823603bab3122a67
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2019-12-09 16:50:34 +0100

    additional documentation

--- include/gbBase/PerfLog.hpp
+++ include/gbBase/PerfLog.hpp
@@ -2,10 +2,10 @@
 #define GHULBUS_LIBRARY_INCLUDE_GUARD_BASE_PERFLOG_HPP
 
 /** @file
-*
-* @brief Minimalistic Performance Logger.
-* @author Andreas Weis (der_ghulbus@ghulbus-inc.de)
-*/
+ *
+ * @brief Minimalistic Performance Logger.
+ * @author Andreas Weis (der_ghulbus@ghulbus-inc.de)
+ */
 
 #include <gbBase/config.hpp>
 #include <gbBase/Log.hpp>
@@ -17,6 +17,11 @@
 
 namespace GHULBUS_BASE_NAMESPACE
 {
+/** A simple performance logger.
+ * PerfLog records timestamped events with every tick().
+ * These can either be retrieved later or passed on to the logger straight away.
+ * Each event consists of a string label and a timestamp.
+ */
 class PerfLog {
     struct LogEntry {
         std::chrono::steady_clock::time_point t;
@@ -30,20 +35,33 @@ class PerfLog {
 private:
     Log m_log;
 public:
+    /** Constructor.
+     * This will create the initial event labeled "Epoch".
+     */
     inline PerfLog()
         :PerfLog("Epoch")
     {}
 
+    /** Constructor.
+     * This will create the initial event with the label passed as argument.
+     */
     inline explicit PerfLog(std::string_view initial_label)
     {
         tick(initial_label);
     }
 
+    /** Adds a new event to the log.
+     * @param[in] label The label associated with the event.
+     */
     void tick(std::string_view label)
     {
         m_log.emplace_back(std::chrono::steady_clock::now(), std::string(label));
     }
 
+    /** Adds a new event to the log and log the time passed since the last tick to the logger.
+     * @param[in] log_level The log level used for the triggered logging operation.
+     * @param[in] label The label associated with the event.
+     */
     void tick(LogLevel log_level, std::string_view label)
     {
         auto const t0 = m_log.back().t;
@@ -52,6 +70,8 @@ public:
             std::chrono::duration_cast<std::chrono::milliseconds>(m_log.back().t - t0).count() << " ms.");
     }
 
+    /** Retrieves the event log.
+     */
     Log const& getLog() const
     {
         return m_log;
